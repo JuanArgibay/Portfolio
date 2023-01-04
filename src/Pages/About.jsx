6e6d8eb987045d77245628211948a10f8427ad4f
ArgyDev <jmargibayg@gmail.com>
2023-01-04 18:23:58 +0100

    open linkedin and github profile in a new window

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -19,10 +19,10 @@ export const About = () => {
             <h1>¡Hola! Soy, <br/> <span id='iam' className={darkMode}>Juan Argibay</span> <br/> Web Developer.</h1>
             <ul>
               <li>
-                <a title='Perfil de Linkedin' href='https://www.linkedin.com/in/juan-m-argibay-gonzalez/'>{darkMode === 'light'? <LinkedinLogo/> : <LinkedinLogoDark/>}</a>
+                <a title='Perfil de Linkedin' target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/juan-m-argibay-gonzalez/'>{darkMode === 'light'? <LinkedinLogo/> : <LinkedinLogoDark/>}</a>
               </li>
               <li>
-                <a title='Repositorio de GitHub' href='https://github.com/JuanArgibay'>{darkMode === 'light'? <GitHubLogo/> : <GitHubLogoDark/>}</a>
+                <a title='Repositorio de GitHub' target="_blank" rel='noreferrer' href='https://github.com/JuanArgibay'>{darkMode === 'light'? <GitHubLogo/> : <GitHubLogoDark/>}</a>
               </li>
             </ul>
           </div>
@@ -30,7 +30,7 @@ export const About = () => {
           <p>Soy una persona curiosa, activa y con ganas de aprender. Apasionado de las nuevas tecnologías, herramientas y formas para hacer mejor las cosas. Disfruto trabajando en equipo intercambiando ideas y resolviendo problemas. </p>
             <div className='descriptionLinks-container'>
               <div className='descriptionLinks'>
-                <a href={CV} title='descargar CV' className={darkMode} download>Descargar CV</a>
+                <a href={CV} title='Descargar CV' className={darkMode} download>Descargar CV</a>
                 -
                 <a type='email' title='Contáctame' href='mailto:jmargibayg@gmail.com' className={darkMode}>Contáctame</a>
               </div>
