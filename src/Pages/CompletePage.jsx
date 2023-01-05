07e33fd51eb661ef08981b8004e352619d94719b
ArgyDev <jmargibayg@gmail.com>
2023-01-06 00:20:40 +0100

    responsive styles done

--- src/Pages/CompletePage.jsx
+++ src/Pages/CompletePage.jsx
@@ -6,23 +6,24 @@ import { Projects } from "./Projects"
 import { Skills } from "./Skills"
 
 
+
 export const CompletePage = () => {
 
     const { darkMode } = useContext(DarkContext);
 
-  return (
+    return (
         <div id='test' className={darkMode}>
             <div id="app-container" className={darkMode}>
-                <Header/>
+                <Header />
                 <main>
                     <div className="decorationBar-container">
-                        <div className={darkMode} id="decorationBar"/>
+                        <div className={darkMode} id="decorationBar" />
                     </div>
-                    <About/>
-                    <Skills/>
-                    <Projects/>
+                    <About />
+                    <Skills />
+                    <Projects />
                 </main>
             </div>
         </div>
-  )
+    )
 }
--- src/Pages/Projects.jsx
+++ src/Pages/Projects.jsx
@@ -5,95 +5,95 @@ import { useContext } from 'react'
 import { DarkContext } from '../context/darkMode'
 export const Projects = () => {
 
-  const {darkMode} = useContext(DarkContext);
-  
+  const { darkMode } = useContext(DarkContext);
+
   return (
     <section id="projects" className={darkMode}>
-        <div className='projectsPageContainer'>
-          <h2 className='titlePage'>Proyectos</h2>
-          <div className='projects-container'>
-             <article className='projectCard' >
-                <div className='card-content'> 
-                <img src={parejas} alt='Parejas'></img>
-                  <ul className='projectSkills'>
-                    <li className='html'>
-                      HTML
-                    </li>
-                    <li className='css'>
-                      CSS
-                    </li>
-                    <li className='js'>
-                      JavaScript
-                    </li>
-                  </ul>
-                  <h3>Descubre la pareja</h3>
-                    <div className='projectLinksContainer'>
-                      <a className={darkMode} target="_blank" rel='noreferrer' href='https://github.com/JuanArgibay/Encuentra-la-pareja'>Github</a>
-                      <a className={darkMode} target="_blank" rel='noreferrer' href='https://encuentralapareja.vercel.app/'>Website</a>
-                    </div>
-                  <details>
-                    <summary></summary>
-                    <p className='projectDescription'>Juego de memoria para los más pequeños de la casa. Agiliza tu mente con esta app y encuentra las ocho parejas.</p>
-                  </details>
-                </div>
-            </article>
-            <article className='projectCard'>
-              <div className='card-content'>
-                <img  src={instagram} alt='instagram'></img>
-                  <ul className='projectSkills'>
-                    <li className='react'>
-                      React.js
-                    </li>
-                    <li className='express'>
-                      Express
-                    </li>
-                    <li className='node'>
-                      Node.js
-                    </li>
-                  </ul>
-                  <h3>Instagram Pro</h3>
-                  <div >
-                    <div className='projectLinksContainer'>
-                      <a className={darkMode} target="_blank" rel='noreferrer' href='https://github.com/JuanArgibay/Instagram-Pro-FrontEnd'>Github</a>
-                      <a className={darkMode} target="_blank" rel='noreferrer' href='https://www.youtube.com/watch?v=cF04GUGUXTo&t=1s'>Website</a>
-                    </div>
-                    <details>
-                      <summary></summary>
-                      <p className='projectDescription'>Aplicación inspirada en Instagram. Comparte tus momentos, sube, comenta y dale like a cuantas fotos quieras.</p>
-                  </details>
-                </div>
+      <div className='projectsPageContainer'>
+        <h2 className='titlePage'>Proyectos</h2>
+        <div className='projects-container'>
+          <article className='projectCard' >
+            <div className='card-content'>
+              <img src={parejas} alt='Parejas'></img>
+              <ul className='projectSkills'>
+                <li className='html'>
+                  HTML
+                </li>
+                <li className='css'>
+                  CSS
+                </li>
+                <li className='js'>
+                  JavaScript
+                </li>
+              </ul>
+              <h3>Descubre la pareja</h3>
+              <div className='projectLinksContainer'>
+                <a className={darkMode} target="_blank" rel='noreferrer' href='https://github.com/JuanArgibay/Encuentra-la-pareja'>Github</a>
+                <a className={darkMode} target="_blank" rel='noreferrer' href='https://encuentralapareja.vercel.app/'>Website</a>
               </div>
-            </article>
-            <article className='projectCard'>
-              <div className='card-content'>
-                <img src={poke} alt='pokeApi'></img>
-                  <ul className='projectSkills'>
-                    <li className='html'>
-                      HTML
-                    </li>
-                    <li className='css'>
-                      CSS
-                    </li>
-                    <li className='react'>
-                      React.js
-                    </li>
-                  </ul>
-                  <h3>Poke App</h3>
-                  <div>
-                    <div className='projectLinksContainer'>
-                      <a className={darkMode} target="_blank" rel='noreferrer' href='https://github.com/JuanArgibay/PokeApp'>Github</a>
-                      <a className={darkMode} target="_blank" rel='noreferrer' href='https://poke-app-cyny.vercel.app/'>Website</a>
-                    </div>
-                    <details>
-                    <summary></summary>
-                    <p className='projectDescription'>App para todos los amantes de Pokémon, podrás encontrarlos a todos junto con sus características y habilidades de una forma muy visual.</p>
-                  </details>
+              <details>
+                <summary></summary>
+                <p className='projectDescription'>Juego de memoria para los más pequeños de la casa. Agiliza tu mente con esta app y encuentra las ocho parejas.</p>
+              </details>
+            </div>
+          </article>
+          <article className='projectCard'>
+            <div className='card-content'>
+              <img src={instagram} alt='instagram'></img>
+              <ul className='projectSkills'>
+                <li className='react'>
+                  React.js
+                </li>
+                <li className='express'>
+                  Express
+                </li>
+                <li className='node'>
+                  Node.js
+                </li>
+              </ul>
+              <h3>Instagram Pro</h3>
+              <div >
+                <div className='projectLinksContainer'>
+                  <a className={darkMode} target="_blank" rel='noreferrer' href='https://github.com/JuanArgibay/Instagram-Pro-FrontEnd'>Github</a>
+                  <a className={darkMode} target="_blank" rel='noreferrer' href='https://www.youtube.com/watch?v=cF04GUGUXTo&t=1s'>Website</a>
                 </div>
+                <details>
+                  <summary></summary>
+                  <p className='projectDescription'>Aplicación inspirada en Instagram. Comparte tus momentos, sube, comenta y dale like a cuantas fotos quieras.</p>
+                </details>
               </div>
-              
-            </article>
-          </div>
+            </div>
+          </article>
+          <article className='projectCard'>
+            <div className='card-content'>
+              <img src={poke} alt='pokeApi'></img>
+              <ul className='projectSkills'>
+                <li className='html'>
+                  HTML
+                </li>
+                <li className='css'>
+                  CSS
+                </li>
+                <li className='react'>
+                  React.js
+                </li>
+              </ul>
+              <h3>Poke App</h3>
+              <div>
+                <div className='projectLinksContainer'>
+                  <a className={darkMode} target="_blank" rel='noreferrer' href='https://github.com/JuanArgibay/PokeApp'>Github</a>
+                  <a className={darkMode} target="_blank" rel='noreferrer' href='https://poke-app-cyny.vercel.app/'>Website</a>
+                </div>
+                <details>
+                  <summary></summary>
+                  <p className='projectDescription'>App para todos los amantes de Pokémon, podrás encontrarlos a todos junto con sus características y habilidades de una forma muy visual.</p>
+                </details>
+              </div>
+            </div>
+
+          </article>
         </div>
+      </div>
     </section>
   )
 }
